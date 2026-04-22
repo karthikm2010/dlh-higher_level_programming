06f3cbaf6a226d14401c426d63ef1ba3ab026081
Karthikeyan Marimuthu <karthikm2010@gmail.com>
2026-04-22 18:02:59 +0000

    8 script updated

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
         if 97 <= ord(c) <= 122:
             print("{}".format(chr(ord(c) - 32)), end="")
         else:
-            print("{}".format(c), end="")
+            print(c, end="")
     print()

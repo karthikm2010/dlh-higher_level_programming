fdd18935ebc24d8902bc164b9838465e6caf569e
Karthikeyan Marimuthu <karthikm2010@gmail.com>
2026-04-22 18:28:03 +0000

    9 script partially completed

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = number % 10
+    print(last_digit)
     if number < 0 and last_digit != 0:
         last_digit -= 10
     print("{}".format(last_digit), end="")

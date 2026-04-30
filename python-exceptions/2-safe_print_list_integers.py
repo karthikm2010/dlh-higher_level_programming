6f16e448a6ad24afd495a67420823f5013fb464d
Karthikeyan Marimuthu <karthikm2010@gmail.com>
2026-04-30 09:52:26 +0000

    2 script check for team help

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    # Print the first x elements of a list and only integers.
     count = 0 # number of integers printed
     for i in range(x):
         try:
